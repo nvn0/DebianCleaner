bd744d111aec1935b420ba1508e72f59190059ed
Nuno <75589883+nuno-src@users.noreply.github.com>
2022-10-11 01:12:56 +0100

    Update DebianCleaner.sh

--- DebianCleaner.sh
+++ DebianCleaner.sh
@@ -5,13 +5,13 @@ df -h | grep sda
 sudo apt autoremove && sudo apt autoclean
 sudo apt clean
 
-if [-d ~/.local/share/lbry/lbrynet/blobfiles];
+if [ -d "~/.local/share/lbry/lbrynet/blobfiles" ];
 then
   sudo rm ~/.local/share/lbry/lbrynet/blobfiles/*
 fi
 
 # 2nd option:
-# [-d ~/.local/share/lbry/lbrynet/blobfiles] && sudo rm ~/.local/share/lbry/lbrynet/blobfiles/*
+# [ -d "~/.local/share/lbry/lbrynet/blobfiles" ] && sudo rm ~/.local/share/lbry/lbrynet/blobfiles/*
 
 
 sudo rm -r /var/log/journal/*
